5920ca2dca63b0425011edbe39ba084744542175
NwabuezeDavid <131411341+nwabuezedavid@users.noreply.github.com>
2025-11-26 09:36:47 -0500

    Update email host and user for production settings

--- banking/settings.py
+++ banking/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6mjf3!2h2hm9mbscg!m&4upjui(j*ojnev@gmas2jm-b*fboa%'
  
 # SECURITY WARNING: don't run with debug turned on in production!
-EMAIL_HOST = 'chestburgbank.com'
+EMAIL_HOST = 'bentugaltd.com'
   
-EMAIL_HOST_USER = "support@chestburgbank.com" 
+EMAIL_HOST_USER = "support.commersb.com@bentugaltd.com" 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_PASSWORD = "nwabueze666$" 
 EMAIL_PORT = 465 
