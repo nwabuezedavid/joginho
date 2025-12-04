19f5ef172b849a02802c42881beafa886834edcb
unknown <davidnwabueze333@gmail.com>
2025-12-04 11:13:02 -0800

    first commit

---
+++ FILES/main/logo.js
@@ -0,0 +1,6 @@
+/*
+ Copyright(c) 2025 TransUnion LLC. All Rights Reserved. 80808bae-d4c3-47ac-9929-5d77264d823c
+*/
+(function(){
+(function c(){var a=window,b=a.io_global_object_name||"IGLOO";a=a[b]=a[b]||{};a=a.io=a.io||{};b=a.io_ddp;if(a.logoMain)return!1;a.logoMain=c;a.logoVer="5.11.0";b&&b._if_ubb&&(b._CTOKEN="DvGJ8NMhZRg+uP68vs7rf8autYyyEy7HzmihptaIyAc=",b._if_ubb());try{a.api.io_bb.add("LID","IRYG6K76ZPLJwA5gDTb/og")}catch(d){}})();
+}).call(this);
